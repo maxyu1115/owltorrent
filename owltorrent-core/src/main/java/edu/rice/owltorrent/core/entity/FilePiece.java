c267f713cb804459b77ed19a3cd2067f1c9fe21a
yuchengu <guyuchen0212@gmail.com>
2021-09-20 13:06:17 -0500

    Add fields for Torrent and FilePiece Pojo class

--- owltorrent-core/src/main/java/edu/rice/owltorrent/core/entity/FilePiece.java
+++ owltorrent-core/src/main/java/edu/rice/owltorrent/core/entity/FilePiece.java
@@ -2,5 +2,23 @@ package edu.rice.owltorrent.core.entity;
 
 import lombok.Data;
 
+/** Represent the FilePiece class. */
 @Data
-public class FilePiece {}
+public class FilePiece {
+  private final int index;
+  private final long length;
+  private final byte[] hash;
+
+  /**
+   * Initialize a piece
+   *
+   * @param index The piece index in the torrent.
+   * @param length The piece's length,in bytes
+   * @param hash The piece's 20-byte SHA1 hash.
+   */
+  public FilePiece(int index, long length, byte[] hash) {
+    this.index = index;
+    this.length = length;
+    this.hash = hash;
+  }
+}
--- owltorrent-core/src/main/java/edu/rice/owltorrent/core/entity/Torrent.java
+++ owltorrent-core/src/main/java/edu/rice/owltorrent/core/entity/Torrent.java
@@ -2,8 +2,75 @@ package edu.rice.owltorrent.core.entity;
 
 import lombok.Data;
 
+/** Represent the Torrent class after parsing. */
 @Data
 public class Torrent {
   // FIXME: temporary for hello world
-  private final String peerIPAddress;
+  private String peerIPAddress;
+
+  /** The URL of the tracker. */
+  private String announceURL;
+
+  /** A UTF-8 encoded string which is the suggested name to save the file (or directory) as. */
+  private String name;
+
+  /** The number of bytes in each piece the file is split into */
+  private int pieceLength;
+
+  /**
+   * A string whose length is a multiple of 20. It is to be subdivided into strings of length 20,
+   * each of which is the SHA1 hash of the piece at the corresponding index.
+   */
+  private byte[] pieces;
+
+  /** The length of the file, in bytes. */
+  private int length;
+
+  // FIXME: temporary for hello world
+  public Torrent(String peerIPAddress) {
+    this.peerIPAddress = peerIPAddress;
+  }
+
+  // TODO: Add constructor
+  public Torrent() {}
+
+  public String getAnnounceURL() {
+    return announceURL;
+  }
+
+  public void setAnnounceURL(String announceURL) {
+    this.announceURL = announceURL;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getPieceLength() {
+    return pieceLength;
+  }
+
+  public void setPieceLength(int pieceLength) {
+    this.pieceLength = pieceLength;
+  }
+
+  public byte[] getPieces() {
+    return pieces;
+  }
+
+  public void setPieces(byte[] pieces) {
+    this.pieces = pieces;
+  }
+
+  public int getLength() {
+    return length;
+  }
+
+  public void setLength(int length) {
+    this.length = length;
+  }
 }
