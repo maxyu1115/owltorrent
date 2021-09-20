37fb533eb17b4c2f9820d72f1c511ad42b67673c
yuchengu <guyuchen0212@gmail.com>
2021-09-20 15:30:10 -0500

    Add Torrent

--- owltorrent-common/src/main/java/edu/rice/owltorrent/common/entity/Torrent.java
+++ owltorrent-common/src/main/java/edu/rice/owltorrent/common/entity/Torrent.java
@@ -1,9 +1,35 @@
 package edu.rice.owltorrent.common.entity;
 
+import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * Represent the Torrent class after parsing.
+ *
+ * @author yuchengu
+ */
 @Data
+@AllArgsConstructor
 public class Torrent {
-  // FIXME: temporary for hello world
-  private final String peerIPAddress;
+
+  /** The URL of the tracker. */
+  private String announceURL;
+
+  /** A UTF-8 encoded string which is the suggested name to save the file (or directory) as. */
+  private String name;
+
+  /** The number of bytes in each piece the file is split into */
+  private int pieceLength;
+
+  /**
+   * A string whose length is a multiple of 20. It is to be subdivided into strings of length 20,
+   * each of which is the SHA1 hash of the piece at the corresponding index.
+   */
+  private List<String> pieces;
+
+  /** The length of the file, in bytes. */
+  private int length;
+
+  public Torrent() {}
 }
