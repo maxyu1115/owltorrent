9b9202c50a0e906f382e0a38605005131e93d9b8
bhavrish <bhavrish@gmail.com>
2021-10-22 16:34:59 -0500

    Complete UDP tracker integration

--- owltorrent-network/src/main/java/edu/rice/owltorrent/network/PeerLocator.java
+++ owltorrent-network/src/main/java/edu/rice/owltorrent/network/PeerLocator.java
@@ -3,14 +3,16 @@ package edu.rice.owltorrent.network;
 import com.dampcake.bencode.BencodeInputStream;
 import com.google.common.io.ByteStreams;
 import edu.rice.owltorrent.common.entity.Torrent;
+import lombok.NonNull;
+
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.*;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import lombok.NonNull;
 
 /**
  * Tracker connector to fetch peers.
@@ -38,12 +40,16 @@ public class PeerLocator {
     String announceUrl = torrent.getAnnounceURL();
     String protocol = announceUrl.split(":")[0];
 
-    if (protocol.equals("http")) {
-      return locateWithHTTPTracker(torrent);
-    } else if (protocol.equals("udp")) {
-      return locateWithUDPTracker(torrent);
-    } else {
-      System.out.println("Unsupported protocol");
+    try {
+      if (protocol.equals("http")) {
+        return locateWithHTTPTracker(torrent);
+      } else if (protocol.equals("udp")) {
+        return locateWithUDPTracker(torrent);
+      } else {
+        System.out.println("Unsupported protocol");
+        return null;
+      }
+    } catch (Exception e) {
       return null;
     }
   }
@@ -54,59 +60,44 @@ public class PeerLocator {
    * @param torrent Torrent object
    * @return list of peer addresses
    */
-  public List<InetSocketAddress> locateWithHTTPTracker(@NonNull Torrent torrent) {
+  public List<InetSocketAddress> locateWithHTTPTracker(@NonNull Torrent torrent) throws IOException {
     List<InetSocketAddress> addresses = new ArrayList<>();
 
-    try {
-      // Build request
-      String baseURL = "https://torrent.ubuntu.com/announce?"; // TODO: retrieve from torrent
-      String request =
-          baseURL
-              + "info_hash="
-              + infoHash
-              + "&peer_id="
-              + peerID
-              + "&port="
-              + port
-              + "&left="
-              + left
-              + "&downloaded="
-              + downloaded
-              + "&uploaded="
-              + uploaded
-              + "&compact="
-              + compact;
+    String baseURL = torrent.getAnnounceURL();
+    String request =
+        baseURL
+            + "info_hash="
+            + infoHash
+            + "&peer_id="
+            + peerID
+            + "&port="
+            + port
+            + "&left="
+            + left
+            + "&downloaded="
+            + downloaded
+            + "&uploaded="
+            + uploaded
+            + "&compact="
+            + compact;
 
-      // Set up connection with tracker
-      URL url = new URL(request);
-      URLConnection connection = url.openConnection();
+    // Set up connection with tracker
+    URL url = new URL(request);
+    URLConnection connection = url.openConnection();
 
-      // Read bytes from tracker
-      InputStream is = connection.getInputStream();
-      byte[] bytes = ByteStreams.toByteArray(is);
+    // Read bytes from tracker
+    InputStream is = connection.getInputStream();
+    byte[] bytes = ByteStreams.toByteArray(is);
 
-      // Bencode tracker response
-      ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
-      BencodeInputStream bencodeInputStream = new BencodeInputStream(inputStream);
-      var dict = bencodeInputStream.readDictionary();
+    // Bencode tracker response
+    ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
+    BencodeInputStream bencodeInputStream = new BencodeInputStream(inputStream);
+    var dict = bencodeInputStream.readDictionary();
 
-      // Extract peers from bencoded dictionary.
-      String peers = (String) dict.get("peers");
-      byte[] peersAsBytes = peers.getBytes();
-
-      // Iterate peers and store IP + port for each peer
-      for (int i = 0; i < peersAsBytes.length - 6; i += 6) {
-        byte[] ipAsBytes = Arrays.copyOfRange(peersAsBytes, i, i + 4);
-        InetAddress inetAddress = InetAddress.getByAddress(ipAsBytes);
-
-        int peerPort = ((peersAsBytes[i + 5] & 0xFF) << 8) | (peersAsBytes[i + 6] & 0xFF);
-
-        InetSocketAddress inetSocketAddress = new InetSocketAddress(inetAddress, peerPort);
-        addresses.add(inetSocketAddress);
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+    // Extract peers from bencoded dictionary.
+    String peers = (String) dict.get("peers");
+    byte[] peersAsBytes = peers.getBytes();
+    addresses = parsePeers(peersAsBytes);
 
     return addresses;
   }
@@ -117,67 +108,145 @@ public class PeerLocator {
    * @param torrent Torrent object
    * @return list of peer addresses
    */
-  public List<InetSocketAddress> locateWithUDPTracker(@NonNull Torrent torrent) {
+  public List<InetSocketAddress> locateWithUDPTracker(@NonNull Torrent torrent) throws IOException, UnknownHostException {
     List<InetSocketAddress> addresses = new ArrayList<>();
 
-    try {
-      // Create socket on any available port.
-      DatagramSocket datagramSocket = new DatagramSocket();
+    // Create socket on any available port.
+    DatagramSocket datagramSocket = new DatagramSocket();
 
-      // Set up Inet URL to connect to.
-      var tracker = URI.create("udp://tracker.openbittorrent.com:80/announce");
-      InetSocketAddress baseURL = new InetSocketAddress(tracker.getHost(), tracker.getPort());
-      System.out.println(baseURL.toString());
+    // Set up Inet URL to connect to.
+    var tracker = URI.create(torrent.getAnnounceURL());
+    InetSocketAddress baseURL = new InetSocketAddress(tracker.getHost(), tracker.getPort());
+    System.out.println(baseURL.toString());
 
-      // Set up connection with tracker.
-      datagramSocket.connect(baseURL);
+    // Set up connection with tracker.
+    datagramSocket.connect(baseURL);
 
-      // Create connectRequestPacket
-      ByteBuffer connectRequestData = ByteBuffer.allocate(16);
-      long protocol_id = 0x41727101980L;
-      int action = 1;
-      int transaction_id = 12345;
-      connectRequestData.putLong(protocol_id);
-      connectRequestData.putInt(action);
-      connectRequestData.putInt(transaction_id);
+    // Create connectRequestPacket
+    ByteBuffer connectRequestData = ByteBuffer.allocate(16);
+    long protocol_id = 0x41727101980L;
+    int action = 1;
+    int transaction_id = 12345;
+    connectRequestData.putLong(protocol_id);
+    connectRequestData.putInt(action);
+    connectRequestData.putInt(transaction_id);
+    DatagramPacket connectRequestPacket =
+        new DatagramPacket(connectRequestData.array(), connectRequestData.capacity(), baseURL);
 
-      DatagramPacket connectRequestPacket =
-          new DatagramPacket(connectRequestData.array(), connectRequestData.capacity(), baseURL);
-      // Create connectResponsePacket
-      byte[] connectResponseData = new byte[16];
-      DatagramPacket connectResponsePacket =
-          new DatagramPacket(connectResponseData, connectResponseData.length);
+    // Create connectResponsePacket
+    byte[] connectResponseData = new byte[65508];
+    DatagramPacket connectResponsePacket =
+        new DatagramPacket(connectResponseData, connectResponseData.length);
 
-      System.out.println("trying to connect to socket...");
+    // Communicate with socket to get connect response
+    byte[] connectResponse = communicateWithSocket(datagramSocket, connectRequestPacket, connectResponsePacket);
+    if (connectResponse == null) {
+      return addresses; // Return empty addresses
+    }
 
-      // Try connecting up to 8 times
-      for (int i = 0; i < 8; i++) {
-        datagramSocket.setSoTimeout(
-            (int) (15000 * Math.pow(2, i))); // Timeout is 15 * 2 ^ n seconds.
-        System.out.println(i + "th attempt trying to connect to tracker.");
+    // Parse connect response
+    int response_transaction_id = ByteBuffer.wrap(Arrays.copyOfRange(connectResponse, 4, 8)).getInt();
+    long connection_id = ByteBuffer.wrap(Arrays.copyOfRange(connectResponse, 8, 16)).getLong();
+    if (response_transaction_id != transaction_id) { // Req and resp transaction id's not match.
+      return addresses; // Return empty addresses
+    }
 
-        try {
-          datagramSocket.send(connectRequestPacket);
-          System.out.println("successfully sent");
-          datagramSocket.receive(connectResponsePacket);
-          break;
-        } catch (SocketTimeoutException s) {
-          if (i == 7) { // Couldn't connect after 8 attempts.
-            return null;
-          }
-          continue;
+    // Create announceRequestPacket
+    ByteBuffer announceRequestData = ByteBuffer.allocate(98);
+    int event = 0;
+    int ip_address = 0;
+    int key = 0;
+    int num_want = -1;
+    announceRequestData.putLong(connection_id);
+    announceRequestData.putInt(action);
+    announceRequestData.putInt(transaction_id);
+    announceRequestData.put(torrent.getInfoHash().getBytes());
+    announceRequestData.put(peerID.getBytes());
+    announceRequestData.putLong(Long.parseLong(downloaded));
+    announceRequestData.putLong(Long.parseLong(left));
+    announceRequestData.putLong(Long.parseLong(uploaded));
+    announceRequestData.putInt(event);
+    announceRequestData.putInt(ip_address);
+    announceRequestData.putInt(key);
+    announceRequestData.putInt(num_want);
+    DatagramPacket announceRequestPacket =
+            new DatagramPacket(connectRequestData.array(), connectRequestData.capacity(), baseURL);
+
+    // Create announceResponsePacket
+    byte[] announceResponseData = new byte[65508];
+    DatagramPacket announceResponsePacket =
+            new DatagramPacket(announceResponseData, announceResponseData.length);
+
+    // Communicate with socket to get announce response
+    byte[] announceResponse = communicateWithSocket(datagramSocket, announceRequestPacket, announceResponsePacket);
+    if (announceResponse == null) {
+      return addresses; // Return empty addresses
+    }
+
+    // Parse peers from announce response
+    byte[] peersAsBytes = Arrays.copyOfRange(announceResponse, 20, announceResponse.length);
+    addresses = parsePeers(peersAsBytes);
+
+    return addresses;
+  }
+
+  /**
+   * Communicate with tracker socket
+   *
+   * @param datagramSocket socket we are talking with
+   * @param requestPacket request input parameters
+   * @param responsePacket response packet structure
+   * @return socket response
+   */
+  public byte[] communicateWithSocket(@NonNull DatagramSocket datagramSocket, @NonNull DatagramPacket requestPacket, @NonNull DatagramPacket responsePacket) throws IOException {
+    System.out.println("trying to connect to socket...");
+
+    // Try connecting up to 8 times
+    for (int i = 0; i < 8; i++) {
+      datagramSocket.setSoTimeout(
+              (int) (15000 * Math.pow(2, i))); // Timeout is 15 * 2 ^ n seconds.
+      System.out.println(i + "th attempt trying to connect to tracker.");
+
+      try {
+        datagramSocket.send(requestPacket);
+        System.out.println("successfully sent");
+        datagramSocket.receive(responsePacket);
+        break;
+      } catch (SocketTimeoutException s) {
+        if (i == 7) { // Couldn't connect after 8 attempts.
+          return null;
         }
+        continue;
       }
+    }
 
-      System.out.println("got back response from socket!");
+    byte[] response = responsePacket.getData();
+    return response;
+  }
 
-      byte[] connectResponse = connectResponsePacket.getData();
-      System.out.println(connectResponse);
+  /**
+   * Parse peers from byte array
+   *
+   * @param peersAsBytes Peers byte array
+   * @return list of IP addresses
+   */
+  public List<InetSocketAddress> parsePeers(@NonNull byte[] peersAsBytes) throws UnknownHostException {
+    List<InetSocketAddress> addresses = new ArrayList<>();
 
-      // TODO: announce connection
+    // Iterate peers and store IP + port for each peer
+    for (int i = 0; i < peersAsBytes.length - 6; i += 6) {
+      byte[] ipAsBytes = Arrays.copyOfRange(peersAsBytes, i, i + 4);
+      InetAddress inetAddress = InetAddress.getByAddress(ipAsBytes);
 
-    } catch (Exception e) {
-      e.printStackTrace();
+      int peerPort = ((peersAsBytes[i + 5] & 0xFF) << 8) | (peersAsBytes[i + 6] & 0xFF);
+
+      InetSocketAddress inetSocketAddress = new InetSocketAddress(inetAddress, peerPort);
+
+      if (inetSocketAddress.toString().equals("/0.0.0.0:0")) {
+        break;
+      }
+      System.out.println(inetSocketAddress.toString());
+      addresses.add(inetSocketAddress);
     }
 
     return addresses;
--- owltorrent-network/src/test/java/edu/rice/owltorrent/network/PeerLocatorTest.java
+++ owltorrent-network/src/test/java/edu/rice/owltorrent/network/PeerLocatorTest.java
@@ -1,27 +1,70 @@
 package edu.rice.owltorrent.network;
 
 import edu.rice.owltorrent.common.entity.Torrent;
+import edu.rice.owltorrent.common.entity.TwentyByteId;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import static junit.framework.TestCase.fail;
 
 public class PeerLocatorTest {
 
   @Test
   public void locateWithHTTPTracker() {
     Torrent torrent = new Torrent();
-    PeerLocator peerLocator = new PeerLocator();
-    peerLocator.locateWithHTTPTracker(torrent);
+    torrent.setAnnounceURL("https://torrent.ubuntu.com/announce?");
+
+    try {
+      PeerLocator peerLocator = new PeerLocator();
+      peerLocator.locateWithHTTPTracker(torrent);
+    } catch (Exception e) {
+      fail();
+    }
   }
 
   @Test
   public void locateWithUDPTracker() {
     Torrent torrent = new Torrent();
-    PeerLocator peerLocator = new PeerLocator();
-    peerLocator.locateWithUDPTracker(torrent);
+    torrent.setInfoHash(new TwentyByteId(TorrentManager.hexStringToByteArray("2b692a9c1aff75c54729ba129a3c94d2ea5d2b8c")));
+    torrent.setAnnounceURL("udp://tracker.openbittorrent.com:80/announce");
+
+    try {
+      PeerLocator peerLocator = new PeerLocator();
+      peerLocator.locateWithUDPTracker(torrent);
+    } catch (Exception e) {
+      fail();
+    }
   }
 
   @Test(expected = NullPointerException.class)
   public void locatePeers_nullInput() {
     PeerLocator peerLocator = new PeerLocator();
     peerLocator.locatePeers(null);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void locateWithHTTPTracker_nullInput() throws IOException {
+    PeerLocator peerLocator = new PeerLocator();
+    peerLocator.locateWithHTTPTracker(null);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void locateWithUDPTracker_nullInput() throws IOException {
+    PeerLocator peerLocator = new PeerLocator();
+    peerLocator.locateWithUDPTracker(null);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void communicateWithSocket_nullInput() throws IOException {
+    PeerLocator peerLocator = new PeerLocator();
+    peerLocator.communicateWithSocket(null, null, null);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void parsePeers_nullInput() throws UnknownHostException {
+    PeerLocator peerLocator = new PeerLocator();
+    peerLocator.parsePeers(null);
   }
 }
