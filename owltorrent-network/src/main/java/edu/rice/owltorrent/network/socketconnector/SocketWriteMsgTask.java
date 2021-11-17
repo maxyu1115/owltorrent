b9b444b3ea7dad22892243d6520f91ec805751a6
Max Yu <maxyu1115@yahoo.com>
2021-11-17 15:19:22 -0600

    Minor fixes

--- owltorrent-network/src/main/java/edu/rice/owltorrent/network/socketconnector/SocketWriteMsgTask.java
+++ owltorrent-network/src/main/java/edu/rice/owltorrent/network/socketconnector/SocketWriteMsgTask.java
@@ -4,9 +4,11 @@ import edu.rice.owltorrent.common.adapters.TaskExecutor.Task;
 import edu.rice.owltorrent.network.PeerMessage;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 /** @author Max Yu */
 @RequiredArgsConstructor
+@Log4j2(topic = "network")
 public class SocketWriteMsgTask implements Task {
   private final SocketConnector socketConnector;
 
@@ -17,7 +19,7 @@ public class SocketWriteMsgTask implements Task {
       if (msg != null) {
         socketConnector.writeMessage(msg);
       } else {
-
+        log.error("Outgoing message queue empty despite SocketWriteMsgTask");
       }
     } catch (IOException ioException) {
       socketConnector.getOutQueue().add(msg);
